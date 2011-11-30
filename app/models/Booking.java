36fe5d4b50368dd9c57f2ec59b92fefc031babfb
metermaid <rita@ritacheng.com>
2011-11-29 20:13:28 -0800

    Cleaned up some things-- removed initialization stuff from the @Before @_@

--- app/models/Booking.java
+++ app/models/Booking.java
@@ -8,12 +8,14 @@ import play.db.jpa.*;
 public class Booking extends Model {
      
      public Integer	    stallnumber;
+	 public Integer		price;
      public Date	 	startdate;
 	 public Date		enddate;
      public long		merchantid;
     
-     public Booking(Integer _location, Date _startDate, Date _endDate, long _merchant){
+     public Booking(Integer _location, Integer _price, Date _startDate, Date _endDate, long _merchant){
         this.stallnumber = _location;
+        this.price = _price;
         this.startdate = _startDate;
 		this.enddate = _endDate;
         this.merchantid = _merchant;
